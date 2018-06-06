c928a8a85587d94ea91c638b7c37471b3e53f617
Tan Nguyen <nguyentanuit@gmail.com>
2018-06-07 01:40:17 +0700

    feat: add list detail information  with animated for map

--- src/components/map/fakeData.js
+++ src/components/map/fakeData.js
@@ -2,6 +2,9 @@ export default (fakeData = [
   {
     type: 'blue',
     price: '1 tỷ',
+    title: 'Chung cư 1',
+    description: 'Chi tiết chung cư 1',
+    image: 'https://i.imgur.com/sNam9iJ.jpg',
     coordinate: {
       latitude: 10.8941183,
       longitude: 106.7719983
@@ -10,6 +13,9 @@ export default (fakeData = [
   {
     type: 'orange',
     price: '1.5 tỷ',
+    title: 'Chung cư 2',
+    description: 'Chi tiết chung cư 2',
+    image: 'https://i.imgur.com/N7rlQYt.jpg',
     coordinate: {
       latitude: 10.8929563,
       longitude: 106.7685003
@@ -18,9 +24,23 @@ export default (fakeData = [
   {
     type: 'pink',
     price: '800 triệu',
+    title: 'Chung cư 3',
+    description: 'Chi tiết chung cư 3',
+    image: 'https://i.imgur.com/UDrH0wm.jpg',
     coordinate: {
       latitude: 10.8999086,
       longitude: 106.7703075
+    }
+  },
+  {
+    type: 'blue',
+    price: '3 tỷ',
+    title: 'Chung cư 4',
+    description: 'Chi tiết chung cư 4',
+    image: 'https://i.imgur.com/Ka8kNST.jpg',
+    coordinate: {
+      latitude: 10.8836263,
+      longitude: 106.7802353
     }
   }
 ]);
--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Animated, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -11,68 +11,254 @@ const LATITUDE = 10.8941563;
 const LONGITUDE = 106.7690843;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const CARD_HEIGHT = height / 4;
+const CARD_WIDTH = width - 20;
+const FAKE_DATA_LENGTH = fakeDate.lengthr;
+
+// const styles = StyleSheet.create({
+//   main: {
+//     flex: 1
+//   },
+//   map: {
+//     flex: 1
+//   },
+//   tooltip: {
+//     flexDirection: 'column',
+//     alignSelf: 'flex-start'
+//   },
+//   bubble: {
+//     flex: 0,
+//     flexDirection: 'row',
+//     alignSelf: 'flex-start',
+//     backgroundColor: '#FFFFFF',
+//     paddingHorizontal: 4,
+//     paddingVertical: 2,
+//     borderRadius: 3,
+//     borderWidth: 1,
+//     marginBottom: -4
+//   },
+//   price: {
+//     color: '#000000',
+//     fontSize: 13
+//   },
+//   icon: {
+//     fontSize: 24,
+//     alignSelf: 'center'
+//   }
+// });
 
 const styles = StyleSheet.create({
   main: {
     flex: 1
   },
-  tooltip: {
-    flexDirection: 'column',
-    alignSelf: 'flex-start'
+  map: {
+    flex: 1
   },
-  bubble: {
-    flex: 0,
-    flexDirection: 'row',
-    alignSelf: 'flex-start',
-    backgroundColor: '#FFFFFF',
-    paddingHorizontal: 4,
-    paddingVertical: 2,
-    borderRadius: 3,
-    borderWidth: 1,
-    marginBottom: -4
+  scrollView: {
+    position: 'absolute',
+    bottom: 30,
+    left: 0,
+    right: 0,
+    paddingVertical: 10
   },
-  price: {
-    color: '#000000',
-    fontSize: 13
+  endPadding: {
+    paddingRight: width - CARD_WIDTH
   },
-  icon: {
-    fontSize: 24,
+  card: {
+    padding: 10,
+    elevation: 2,
+    backgroundColor: '#FFF',
+    marginHorizontal: 10,
+    shadowColor: '#000',
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: 'hidden'
+  },
+  cardImage: {
+    flex: 3,
+    width: '100%',
+    height: '100%',
     alignSelf: 'center'
+  },
+  textContent: {
+    flex: 1
+  },
+  cardtitle: {
+    fontSize: 12,
+    marginTop: 5,
+    fontWeight: 'bold'
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: '#444'
+  },
+  markerWrap: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  marker: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: 'rgba(130,4,150, 0.9)'
+  },
+  markerSelected: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#FF9800'
+  },
+  ring: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: 'rgba(130,4,150, 0.3)',
+    position: 'absolute',
+    borderWidth: 1,
+    borderColor: 'rgba(130,4,150, 0.5)'
   }
 });
 
-const renderColor = type => {
-  if (type === 'blue') {
-    return '#2196F3';
-  } else if (type === 'orange') {
-    return '#FF9800';
-  }
-  return '#E91E63';
-};
+// const renderColor = type => {
+//   if (type === 'blue') {
+//     return '#2196F3';
+//   } else if (type === 'orange') {
+//     return '#FF9800';
+//   }
+//   return '#E91E63';
+// };
 
 export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentMarkerIndex: 0,
+      animation: new Animated.Value(0),
+      regionTimeout: 10
+    };
+  }
+
+  componentDidMount() {
+    const { animation, regionTimeout, currentMarkerIndex } = this.state;
+    animation.addListener(({ value }) => {
+      let index = Math.floor(value / CARD_WIDTH + 0.3);
+      if (index <= 0) {
+        index = 0;
+      }
+      if (index >= FAKE_DATA_LENGTH) {
+        index = FAKE_DATA_LENGTH - 1;
+      }
+
+      clearTimeout(regionTimeout);
+      setTimeout(() => {
+        if (currentMarkerIndex !== index) {
+          this.setState({ currentMarkerIndex: index });
+
+          const { coordinate } = fakeDate[index];
+          this.map.animateToRegion(
+            {
+              ...coordinate,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA
+            },
+            350
+          );
+        }
+      }, 10);
+    });
+  }
+
   render() {
+    const { animation } = this.state;
+    const interpolations = fakeDate.map((marker, index) => {
+      const inputRange = [(index - 1) * CARD_WIDTH, index * CARD_WIDTH, (index + 1) * CARD_WIDTH];
+      const scale = animation.interpolate({
+        inputRange,
+        outputRange: [1, 2.5, 1],
+        extrapolate: 'clamp'
+      });
+      const opacity = animation.interpolate({
+        inputRange,
+        outputRange: [0.35, 1, 0.35],
+        extrapolate: 'clamp'
+      });
+      return { scale, opacity };
+    });
+
     return (
-      <MapView
-        style={styles.main}
-        initialRegion={{
-          latitude: LATITUDE,
-          longitude: LONGITUDE,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA
-        }}
-      >
-        {fakeDate.map(item => (
-          <Marker onPress={() => console.log('marker pressed!')} coordinate={item.coordinate}>
-            <View style={styles.tooltip}>
-              <View style={[styles.bubble, { borderColor: renderColor(item.type) }]}>
-                <Text style={styles.price}>{item.price}</Text>
+      <View style={styles.main}>
+        <MapView
+          ref={map => {
+            this.map = map;
+          }}
+          style={styles.map}
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
+          }}
+        >
+          {fakeDate.map((marker, index) => {
+            const scaleStyle = {
+              transform: [
+                {
+                  scale: interpolations[index].scale
+                }
+              ]
+            };
+            const opacityStyle = {
+              opacity: interpolations[index].opacity
+            };
+
+            return (
+              <Marker key={marker.title} coordinate={marker.coordinate}>
+                <Animated.View style={[styles.markerWrap, opacityStyle]}>
+                  <Animated.View style={[styles.ring, scaleStyle]} />
+                  <View style={styles.marker} />
+                </Animated.View>
+              </Marker>
+            );
+          })}
+        </MapView>
+        <Animated.ScrollView
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={CARD_WIDTH}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: animation
+                  }
+                }
+              }
+            ],
+            { useNativeDriver: true }
+          )}
+          style={styles.scrollView}
+          contentContainerStyle={styles.endPadding}
+        >
+          {fakeDate.map(marker => (
+            <View style={styles.card} key={marker.title}>
+              <Image source={{ uri: marker.image }} style={styles.cardImage} resizeMode="cover" />
+              <View style={styles.textContent}>
+                <Text numberOfLines={1} style={styles.cardtitle}>
+                  {marker.title}
+                </Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>
+                  {marker.description}
+                </Text>
               </View>
-              <Icon name="md-pin" style={[styles.icon, { color: renderColor(item.type) }]} />
             </View>
-          </Marker>
-        ))}
-      </MapView>
+          ))}
+        </Animated.ScrollView>
+      </View>
     );
   }
 }
